9633cc538c2c4cabf5003116dd342f71be0124e4
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-06-20 22:34:34 -0600

    Basic code structure

---
+++ common/k2b6s9j/Engines/KeplersEnginesMod.java
@@ -0,0 +1,28 @@
+package k2b6s9j.Engines;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class KeplersEnginesMod {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ common/k2b6s9j/Engines/Reference.java
@@ -0,0 +1,9 @@
+package k2b6s9j.Engines;
+
+public class Reference {
+	
+	public final static String MOD_NAME = "Kepler's Engines";
+	public final static String MOD_ID = "KeplersEngines";
+	public final static String VERSION = "0";
+
+}
