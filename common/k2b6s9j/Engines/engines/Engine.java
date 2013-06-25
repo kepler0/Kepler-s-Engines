a9d5cfa8abf2c5a1e0e95aae2a2882e52a91a09a
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-06-24 23:37:46 -0700

    Now extending BuildCraft's Engine class
    Thankfully, Krapht reassured me that it would be okay if I did.

--- common/k2b6s9j/Engines/engines/Engine.java
+++
@@ -1,5 +0,0 @@
-package k2b6s9j.Engines.engines;
-
-public class Engine {
-
-}
--- common/k2b6s9j/Engines/engines/EngineSolar.java
+++ common/k2b6s9j/Engines/engines/EngineSolar.java
@@ -1,5 +1,70 @@
 package k2b6s9j.Engines.engines;
 
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.liquids.ILiquidTank;
+import net.minecraftforge.liquids.LiquidStack;
+import buildcraft.energy.Engine;
+import buildcraft.energy.TileEngine;
+
 public class EngineSolar extends Engine {
+
+	public EngineSolar(TileEngine tile) {
+		super(tile);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getTextureFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int explosionRange() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int maxEnergyReceived() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getPistonSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isBurning() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getScaledBurnTime(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void burn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ILiquidTank getTank(ForgeDirection direction, LiquidStack type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
